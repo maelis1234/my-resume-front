515b9bb350c1f30c0dc23f5de8bd635c048ed0bf
mhammouche1 <maelis.hammouche@atos.net>
2023-03-20 15:43:55 +0100

    build: init project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '600px',
+      lg: '1024px',
+    },
+    extend: {
+      colors: {
+        'primary-violet': '#6D28D9',
+        'background-violet': '#EED4FA'
+      }
+    },
+  },
+  plugins: [],
+}
