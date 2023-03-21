e5e3185c80f10a84f9ad869874969d7da0946d69
mhammouche1 <maelis.hammouche@atos.net>
2023-03-21 15:33:34 +0100

    feat: add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,43 @@
+import Link from 'next/link'
+
 const Footer = () => {
-    return <div>footer</div>
+    return (
+        <footer className='text-sm p-3 border-t border-t-background-violet'>
+            <div className='flex justify-between '>
+                <div>
+                    <p className='font-bold'>Tel</p>
+                    <p>06 XX XX XX XX</p>
+                </div>
+                <Link
+                    href='mailto:maelis.hammouche@gmail.com'
+                    className='hover:text-primary-violet'
+                >
+                    <p className='font-bold'>Mail</p>
+                    <p>maelis.hammouche@gmail.com</p>
+                </Link>
+            </div>
+            <div className='space-x-4'>
+                <Link
+                    href='https://www.linkedin.com/in/maelis-hammouche/'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                >
+                    <i className='ri-linkedin-fill text-xl hover:text-primary-violet' />
+                </Link>
+                <Link
+                    href='https://github.com/maelis1234'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                >
+                    <i className='ri-github-fill text-xl hover:text-primary-violet' />
+                </Link>
+            </div>
+            <div className='flex pt-2'>
+                <i className='ri-copyright-fill ' />
+                <p>2023 par Maëlis Hammouche</p>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import 'remixicon/fonts/remixicon.css'
 import type { AppProps } from 'next/app'
 import Sidebar from '@/components/sidebar'
+import Footer from '@/components/footer'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Sidebar />
             <Component {...pageProps} />
+            <Footer />
         </>
     )
 }
